6780520d54bd0b19129901ef87dced1a5bfe0a16
Kawsar Hussen <kawsarhussen@gmail.com>
2019-06-20 14:57:43 -0400

    Initial commit, added queue

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -38,7 +38,21 @@ else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(roomGraph) - len(visited_rooms)} unvisited rooms")
 
+class Queue:
+    def __init__(self):
+        self.queue = []
 
+    def size(self):
+        return len(self.queue)
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else: 
+            return None
 
 #######
 # UNCOMMENT TO WALK AROUND
